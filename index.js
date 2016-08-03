2652a033195f8459e6f925eda65f511aaff8b076
Dreu Stepanik <dreuezekiel@gmail.com>
2016-08-03 16:45:40 -0700

    refactored the code

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function calculatedWeight(){
     var userWeight = document.getElementById("userWeight").value;
-    var planetMultiplier = document.getElementById("planetMultiplier").value;              
+
+    var select = document.getElementById('planetMultiplier');
+
+    var options = select.options;
+
+    var selectedOption = options[select.selectedIndex];
+
+    var planetName = selectedOption.innerHTML;
+    var planetMultiplier = selectedOption.value;
+             
 // The function returns the product of userWeight and planetMultiplier
 
 
-	var calculatedWeight = userWeight * planetMultiplier
-	document.getElementById("calculatedWeight").innerHTML = calculatedWeight;
-}
+	var calculatedWeight = userWeight * planetMultiplier;
+	document.getElementById("calculatedWeight").innerHTML = "If you were on " + planetName + " ,you would weigh " + calculatedWeight + "lbs!"
+};
